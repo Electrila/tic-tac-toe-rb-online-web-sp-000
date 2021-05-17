8879ac8fdb3eb23574de756c9cb34fbf0a57a14b
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-17 14:46:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,6 +22,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  input = input.to_i
+  index = input - 1
+end
  
 def won?(board)
   WIN_COMBINATIONS.each { |win_combo|
